bc5e41cc4570c9d685fc2d892fff9741e10ead04
martinliba <martin.liba@hotmail.com>
2019-12-05 13:24:36 +0300

    Updates

---
+++ precache-manifest.3c90b930a1a90c2ac03df4a530a878c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106042b08202ae678e65659f78f66935",
+    "url": "/newsreader/index.html"
+  },
+  {
+    "revision": "7a59c7959da9ae5fa4de",
+    "url": "/newsreader/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "4a281c76bc80830b8962",
+    "url": "/newsreader/static/css/main.ec2f30d7.chunk.css"
+  },
+  {
+    "revision": "7a59c7959da9ae5fa4de",
+    "url": "/newsreader/static/js/2.55ea5a6a.chunk.js"
+  },
+  {
+    "revision": "4a281c76bc80830b8962",
+    "url": "/newsreader/static/js/main.cac9a7f5.chunk.js"
+  },
+  {
+    "revision": "f72836ed0c37285a806b",
+    "url": "/newsreader/static/js/runtime-main.1c015130.js"
+  }
+]);
